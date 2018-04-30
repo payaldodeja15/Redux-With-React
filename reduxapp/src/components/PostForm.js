64730434681365b9b6500dbbce12f4d6642c70cb
payaldodeja15 <dodeja.p@husky.neu.edu>
2018-04-29 22:49:43 -0400

    Adding React props and state

---
+++ reduxapp/src/components/PostForm.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class PostForm extends Component {
+
+
+  constructor(props)
+  {
+    super(props);
+    this.state={
+}
+  }
+  
+  render() {
+    
+    return (
+      <div>
+        <h1>Add Posts</h1>
+        <form>
+            <div>
+                <label>Title:</label><br/>
+                <input type="text" name="title"/>
+
+                </div><br/>
+                <div>
+                <label>Body:</label><br/>
+                <textarea name="body"/>
+                
+                </div>
+                <br/>
+                <button typ="submit">Submit</button>
+            </form>
+       
+      </div>
+    )
+  }
+}
+
+export default PostForm;
